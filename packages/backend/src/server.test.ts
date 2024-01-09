e39486d7e7ac43d40255e53be1b14c7c7dca9148
JasperVandenberghen <jasper.vandenberghen@gmail.com>
2024-01-09 14:08:43 +0100

    test: add server tests

--- packages/backend/src/server.test.ts
+++ packages/backend/src/server.test.ts
@@ -1,10 +1,10 @@
 import request from 'supertest';
 import app from './server';
 
-const testPort = 3001;
+const testPort = process.env.TEST_PORT || 3001;
 let server: any;
 
-describe('Express App Tests', () => {
+describe('Server', () => {
   beforeAll((done) => {
     server = app.listen(testPort, () => {
       console.log(`Test server is running on http://localhost:${testPort}`);
@@ -12,8 +12,22 @@ describe('Express App Tests', () => {
     });
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   afterAll((done) => {
     server.close(done);
+  });
+
+  it('should initialize the server without errors', () => {
+    expect(app).toBeDefined();
+  });
+
+  it('should include CORS middleware', async () => {
+    const response = await request(app).get('/api/quotes/random');
+
+    expect(response.header['access-control-allow-origin']).toBe('*');
   });
 
   it('should respond with status 200 for GET request to /api/quotes/random', async () => {
@@ -24,5 +38,36 @@ describe('Express App Tests', () => {
     expect(quoteData.author).toBeDefined();
     expect(quoteData.tags).toBeDefined();
     expect(quoteData.authorSlug).toBeDefined();
+  });
+
+
+  it('should respond with status 200 and valid JSON data for GET request to /api/quotes/random', async () => {
+    const mockQuoteData = {
+      content: 'This is a test quote',
+      author: 'Test Author',
+      tags: ['test', 'quote'],
+      authorSlug: 'test-author',
+    };
+
+    global.fetch = jest.fn().mockResolvedValueOnce({
+      ok: true,
+      json: async () => mockQuoteData,
+    });
+
+    const response = await request(app).get('/api/quotes/random');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(mockQuoteData);
+  });
+
+
+ 
+  it('should respond with 500 if fetch failed', async () => {
+    global.fetch = jest.fn().mockRejectedValueOnce(new Error('Fetch failed'));
+
+    const response = await request(app).get('/api/quotes/random');
+    console.log(response.error);
+    expect(response.status).toBe(500);
+    expect(response.serverError).toBe(true);
   });
 });

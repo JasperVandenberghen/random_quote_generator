63e58676a594d27c2db3af78dd2b3f57f07f3b67
JasperVandenberghen <jasper.vandenberghen@gmail.com>
2024-01-10 17:18:14 +0100

    tests: add test to app

--- packages/frontend/src/App.test.tsx
+++ packages/frontend/src/App.test.tsx
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+it('renders the title', () => {
   render(<App />);
-  const linkElement = screen.getByText("Click the button for a random quote!");
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText("Click the button for a random quote!");
+  expect(titleElement).toBeInTheDocument();
+});
+
+it('handles button click and fetches quotes', async () => {
+  render(<App />);
+
+  global.fetch = jest.fn().mockResolvedValue({
+    ok: true,
+    json: () => Promise.resolve([{ content: 'Mocked Quote 1' }]),
+  });
+
+  fireEvent.click(screen.getByText("Generate Quote"));
+
+  expect(global.fetch).toHaveBeenCalledTimes(1);
+  expect(global.fetch).toHaveBeenCalledWith('http://localhost:3000/api/quotes/random')
+    
 });
